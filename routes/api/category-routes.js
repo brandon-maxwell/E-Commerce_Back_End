242cc2db8afe55d60be2e50d317206a9f61db225
Brandon Maxwell <brandon.l.maxwell@gmail.com>
2021-04-30 23:17:09 -0500

    finished routes. Moving to testing

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -23,7 +23,7 @@ router.get('/:id', async (req, res) => {
     });
 
     if (!categoryData) {
-      res.status(404).json({ message: 'No category found with this ID' });
+      res.status(404).json({ message: 'No category found with that ID' });
       return;
     }
 
@@ -53,7 +53,7 @@ router.put('/:id', async (req, res) => {
     });
 
     if (!categoryData) {
-      res.status(404).json({ message: 'No category found with this ID' });
+      res.status(404).json({ message: 'No category found with that ID' });
       return;
     }
 
@@ -74,7 +74,7 @@ router.delete('/:id', async (req, res) => {
     });
 
     if (!categoryData) {
-      res.status(404).json({ message: 'No category found with this ID' });
+      res.status(404).json({ message: 'No category found with that ID' });
       return;
     }
 
